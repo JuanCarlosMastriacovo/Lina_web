cd08169816c1f8f7d6148e90d7403e9df6a6a7ef
jcm <mastriacovo@gmail.com>
2026-03-21 08:32:05 -0300

    desde terminal

--- CapaDAL/config.py
+++ CapaDAL/config.py
@@ -11,4 +11,3 @@ MYSQL_CONFIG = {
     "pool_size": int(os.getenv("LINA_MYSQL_POOL_SIZE", "5")),
     "pool_name": os.getenv("LINA_MYSQL_POOL_NAME", "lina_pool"),
 }
-# Configuración de logging para LINA_WEB.
